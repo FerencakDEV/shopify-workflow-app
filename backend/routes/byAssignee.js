1d460967a710f7552b698d42eead976083bfd802
Ján Ferenčák <github@ferencak.dev>
2025-06-06 12:41:24 +0200

    asdf

--- backend/routes/byAssignee.js
+++ backend/routes/byAssignee.js
@@ -11,21 +11,27 @@ router.get('/by-assignee/:name', async (req, res) => {
   try {
     const orders = await Order.find({
       fulfillment_status: excludeFulfilled,
-      $or: [
-        { assignee_1: assigneeName },
-        { assignee_2: assigneeName },
-        { assignee_3: assigneeName },
-        { assignee_4: assigneeName }
-      ],
-      $or: [
-        { progress_1: regex('assigned') },
-        { progress_2: regex('assigned') },
-        { progress_3: regex('assigned') },
-        { progress_4: regex('assigned') },
-        { progress_1: regex('in progress') },
-        { progress_2: regex('in progress') },
-        { progress_3: regex('in progress') },
-        { progress_4: regex('in progress') }
+      $and: [
+        {
+          $or: [
+            { assignee_1: assigneeName },
+            { assignee_2: assigneeName },
+            { assignee_3: assigneeName },
+            { assignee_4: assigneeName }
+          ]
+        },
+        {
+          $or: [
+            { progress_1: regex('assigned') },
+            { progress_2: regex('assigned') },
+            { progress_3: regex('assigned') },
+            { progress_4: regex('assigned') },
+            { progress_1: regex('in progress') },
+            { progress_2: regex('in progress') },
+            { progress_3: regex('in progress') },
+            { progress_4: regex('in progress') }
+          ]
+        }
       ]
     }).select('order_number custom_status fulfillment_status assignee_1 assignee_2 assignee_3 assignee_4');
 
