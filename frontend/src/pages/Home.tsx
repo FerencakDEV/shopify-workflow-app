83efc2af8823d7560be2a5a00bf45b92e777a4ac
Ján Ferenčák <github@ferencak.dev>
2025-06-12 17:04:48 +0200

    fix fr

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -107,10 +107,10 @@ const Home = () => {
 <div
   className={`grid w-full transition-all duration-300 ${
     isFullscreen
-      ? 'grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-[1px] bg-gray-200 min-h-[calc(100vh-64px)]'
+      ? 'grid-cols-2 grid-rows-5 gap-[1px] bg-gray-200 min-h-[calc(100vh-64px)]'
       : 'grid-rows-5 grid-cols-2 gap-4'
   }`}
->          {statusWidgets.map((widget) => (
+>        {statusWidgets.map((widget) => (
               <StatusWidget
                 key={widget.key}
                 statusKey={widget.key}
