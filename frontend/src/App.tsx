97fa417da902eccfc5c2b9f0a87600c1a114199f
Ján Ferenčák <github@ferencak.dev>
2025-06-13 12:18:00 +0200

    fi

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,24 @@
 import React from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Header from "./components/Header";
-import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import WidgetPage from './pages/WidgetPage';
 import OrdersFullscreen from './pages/OrdersFullscreen';
 import WorkloadFullscreen from './pages/WorkloadFullscreen';
 
 function App() {
+  const location = useLocation();
+  const isFullscreen = location.pathname === '/orders/fullscreen' || location.pathname === '/workload/fullscreen';
+
   return (
     <div className="bg-gray-100 min-h-screen">
-      <Header />
+      {!isFullscreen && <Header />}
+
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/status/:slug" element={<WidgetPage />} /> 
+        <Route path="/status/:slug" element={<WidgetPage />} />
         <Route path="/orders/fullscreen" element={<OrdersFullscreen />} />
-<Route path="/workload/fullscreen" element={<WorkloadFullscreen />} />
+        <Route path="/workload/fullscreen" element={<WorkloadFullscreen />} />
       </Routes>
     </div>
   );
