7bc7ccfb216b2167f9e6662c47233762f55d96db
Ján Ferenčák <github@ferencak.dev>
2025-06-13 12:22:54 +0200

    fi

--- frontend/src/pages/WorkloadFullscreen.tsx
+++ frontend/src/pages/WorkloadFullscreen.tsx
@@ -5,12 +5,12 @@ import ContentHeader from '../components/ContentHeader';
 const WorkloadFullscreen: React.FC = () => {
   return (
     <div className="h-screen w-screen flex flex-col bg-white">
-      {/* Sticky header with time + status */}
-      <div className="sticky top-0 z-50">
-        <ContentHeader />
+      {/* Sticky header with time + API status only */}
+      <div className="sticky top-0 z-50 border-b">
+        <ContentHeader hideSearch hideStaff />
       </div>
 
-      {/* Fullscreen content */}
+      {/* Fullscreen Workload content */}
       <div className="flex-grow overflow-auto px-6 py-4">
         <WorkloadChart fullscreen={true} />
       </div>
