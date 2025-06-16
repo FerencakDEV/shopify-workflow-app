3897c42f430746fa1c51ccbaab70139036beb0ba
Ján Ferenčák <github@ferencak.dev>
2025-06-16 22:12:51 +0200

    fix

--- frontend/src/pages/WorkloadFullscreen.tsx
+++ frontend/src/pages/WorkloadFullscreen.tsx
@@ -7,7 +7,16 @@ const WorkloadFullscreen: React.FC = () => {
     <div className="h-screen w-screen flex flex-col bg-white">
       {/* Sticky header with time + API status only */}
       <div className="sticky top-0 z-50 border-b">
-<ContentHeader hideSearch hideStaff rightTitle="Staff Workload" />
+        <ContentHeader
+          hideSearch
+          hideStaff
+          rightTitle={
+            <div className="text-right leading-tight">
+              <div className="text-xl font-bold text-gray-700">Workload</div>
+              <div className="text-sm font-normal text-gray-500">Print &amp; Design</div>
+            </div>
+          }
+        />
       </div>
 
       {/* Fullscreen Workload content */}
